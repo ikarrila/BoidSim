a7a8d0a326a0d99a747c08dbd0d3b2be6c271952
ikarrila <mjunposti@gmail.com>
2023-05-13 00:23:10 +0300

    Structs for ECS

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,40 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
+struct Position {
+    float x;
+    float y;
+};
+
+struct Velocity {
+    float dx;
+    float dy;
+};
+
+struct Boid {
+    Position position;
+    Velocity velocity;
+};
+
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(600, 600), "SFML window");
 
-	while (window.isOpen()) {
-		window.clear(sf::Color::Black);
-		sf::RectangleShape rs(sf::Vector2f(100, 100));
-		window.draw(rs);
-		window.display();
-	}
+    while (window.isOpen())
+    {
+        sf::Event event;
+        window.clear(sf::Color::Black);
+        sf::RectangleShape rs(sf::Vector2f(100, 100));
+        window.draw(rs);
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.display();
+    }
+
+    return 0;
 }
